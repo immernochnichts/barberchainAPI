4f04a66dfe5933bc0fd6d4ce4bfc4e0907701c33
immernochnichts <rubick.ruiner@mail.ru>
2025-09-20 19:20:06 +0300

    Added Review table to the DbContext

---
+++ Data/Review.cs
@@ -0,0 +1,15 @@
+﻿namespace barberchainAPI.Data
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public int AccountId { get; set; }  // fk_account
+
+        public int BarberId { get; set; }   // fk_barber
+
+        public string? Text { get; set; }
+
+        public short? Score { get; set; }   // Score between 0 and 5
+    }
+}
