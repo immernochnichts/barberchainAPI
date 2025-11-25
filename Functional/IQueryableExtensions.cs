7314ff18ef273807e0953e27d7a8a84af504d977
immernochnichts <rubick.ruiner@mail.ru>
2025-11-26 02:24:35 +0300

    As always forgot to add the files. There they are...

---
+++ Functional/IQueryableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace barberchainAPI.Functional
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> OrderByDynamic<T>(this IQueryable<T> query, string prop)
+        {
+            if (string.IsNullOrWhiteSpace(prop)) return query;
+            return query.OrderBy(x => EF.Property<object>(x, prop));
+        }
+
+        public static IQueryable<T> OrderByDescendingDynamic<T>(this IQueryable<T> query, string prop)
+        {
+            if (string.IsNullOrWhiteSpace(prop)) return query;
+            return query.OrderByDescending(x => EF.Property<object>(x, prop));
+        }
+    }
+}
